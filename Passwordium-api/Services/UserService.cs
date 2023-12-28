f790439b09ab7aceab465ad1715a7f82d1175576
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-12-28 21:03:00 +0100

    Added user service

---
+++ Passwordium-api/Services/UserService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Passwordium_api.Data;
+using Passwordium_api.Model.Entities;
+using Passwordium_api.Model.Requests;
+using Passwordium_api.Model.Responses;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Passwordium_api.Services
+{
+    public class UserService
+    {
+        private readonly DatabaseContext _context;
+        private readonly IConfiguration _config;
+
+        public UserService(DatabaseContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _config = configuration;
+        }
+
+        public async Task<LoginResponse> LoginAsync(UserRequest request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == request.Username);
+            if (user == null)
+            {
+                throw new Exception("User not found.");
+            }
+
+            HashService hashService = new HashService();
+            var passwordVerification = hashService.VerifyHashedPassword(user, request.Password);
+            if (passwordVerification == PasswordVerificationResult.Failed)
+            {
+                throw new InvalidDataException("Incorrect password.");
+            }
+
+            TokenService tokenService = new TokenService();
+            string jwt = tokenService.GenerateJwtToken(user, _config);
+            user = tokenService.GenerateRefreshToken(user, _context);
+
+            LoginResponse response = new LoginResponse
+            {
+                JWT = jwt,
+                RefreshToken = user.RefreshToken,
+                RefreshTokenExpiresAt = (DateTime)user.ExpiresAt
+            };
+
+            return response;
+        }
+    }
+}
