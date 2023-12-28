ccce5c0d32584e3f8da084c7c039e777e2871150
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-12-28 21:02:11 +0100

    Added hashing service

---
+++ Passwordium-api/Services/HashService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Passwordium_api.Model.Entities;
+
+namespace Passwordium_api.Services
+{
+    public class HashService
+    {
+        public User HashPassword(User newUser)
+        {
+            var passwordHasher = new PasswordHasher<User>();
+            string hashedPassword = passwordHasher.HashPassword(newUser, newUser.Password);
+            newUser.Password = hashedPassword;
+            return newUser;
+        }
+
+        public PasswordVerificationResult VerifyHashedPassword(User user, string password)
+        {
+            var passwordHasher = new PasswordHasher<User>();
+            return passwordHasher.VerifyHashedPassword(user, user.Password, password);
+        }
+    }
+}
