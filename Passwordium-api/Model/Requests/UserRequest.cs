9f3ac0d485980cfd35afd8e58e1333f5a0fb272d
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2023-12-28 21:02:00 +0100

    Added response and request clasess

---
+++ Passwordium-api/Model/Requests/UserRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Passwordium_api.Model.Requests
+{
+    public class UserRequest
+    {
+        [Required]
+        public required string Username { get; set; }
+        [Required]
+        public required string Password { get; set; }
+    }
+}
---
+++ Passwordium-api/Model/Responses/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Passwordium_api.Model.Responses
+{
+    public class LoginResponse
+    {
+        public required string JWT { get; set; }
+        public required string RefreshToken { get; set; }
+        public required DateTime RefreshTokenExpiresAt { get; set; }
+    }
+}
